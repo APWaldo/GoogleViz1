1b229b6957fcfa41d2f431c344ab2747fba43d37
APWaldo <apw2131@columbia.edu>
2014-02-25 10:33:27 -0500

    Adding document ready to scripts.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
 /**
  * @author
  */
+
+
+console.log ("Hi there");
+
+function pageLoaded () {
+	
+	console.log ("Got to page loaded.");
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(pageLoaded);
