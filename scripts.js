db1e2acc00cfcf16ad61d7018cefbf5eee3259be
APWaldo <apw2131@columbia.edu>
2014-02-25 11:08:08 -0500

    Adding dataLoaded function to scripts.

--- scripts.js
+++ scripts.js
@@ -9,11 +9,15 @@ console.log ("Hi there");
 function googleLoaded(){
 	console.log ("Google has loaded")
 	
+	//Time to load data with get function. After I need to create function dataLoaded.
+	$.get("UEMP270V_data.json", dataLoaded, "json");
+	
 }
 
 function pageLoaded () {
 	
 	console.log ("Got to page loaded.");
+	
 	//Load the google visualization library with the callback googleLoaded.
 	google.load("visualization", "1", {packages:["corechart"], callback: "googleLoaded"});
 	
