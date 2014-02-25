58de7af00e5f1ff4b18c79c862184b836d768236
APWaldo <apw2131@columbia.edu>
2014-02-25 11:01:29 -0500

    Adding googleLoaded function to scripts.

--- scripts.js
+++ scripts.js
@@ -5,20 +5,19 @@
 
 console.log ("Hi there");
 
-function pageLoaded () {
-	
-	console.log ("Got to page loaded.");
+//Add the googleLoaded function.
+function googleLoaded(){
+	console.log ("Google has loaded")
 	
 }
 
-
-
-
-
-
-
-
-
+function pageLoaded () {
+	
+	console.log ("Got to page loaded.");
+	//Load the google visualization library with the callback googleLoaded.
+	google.load("visualization", "1", {packages:["corechart"], callback: "googleLoaded"});
+	
+}
 
 
 
